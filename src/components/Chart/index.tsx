c15a7a812ee8087540df13a3e64a0aa7daf54508
luan.staner <luan.staner@gmail.com>
2021-04-23 19:55:11 -0300

    Configuring charts js

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,36 +1,56 @@
+import { ChartData } from 'chart.js';
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import "./styles.css"
+import { generateColor, randomNumber } from '../../services/utils';
+import options from './options';
+import "./styles.css";
 
-const Chart = () =>{
-    const dataCharts = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-            {
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
+const Chart = () => {
+
+    const labels = ['Red', 'Blue',]
+
+    const datasets = [
+        {
+            label: '# of Votes 1',
+            data: [
+                randomNumber(),
+                randomNumber(),
+            ],
+
+        },
+        {
+            label: '# of Votes 2',
+            data: [
+                randomNumber(),
+                randomNumber(),
+            ],
+        }
+    ]
+
+    const datasetsSerialized = datasets.map(dataset => {
+        const color = generateColor();
+        return {
+            ...dataset,
+            pointBackgroundColor: color.rgb,
+            backgroundColor: color.rgb,
+            borderColor: color.rgb,
+            borderWidth: 1,
+        }
+    })
+
+    const dataCharts: ChartData = {
+        labels,
+        datasets: datasetsSerialized
     };
-    return(
-        <Line id="graph-chart" data={dataCharts} type="line"  className="chart" />
+
+    return (
+        <Line
+            id="graph-chart"
+            data={dataCharts}
+            type="line"
+            className="chart"
+            options={options}
+        />
     )
 }
 
---
+++ src/components/Chart/options.ts
@@ -0,0 +1,30 @@
+import { ChartOptions } from "chart.js";
+
+const options: ChartOptions = {
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            display: true,
+            position: "right",
+            reverse: false,
+            align: 'start',
+            labels: {
+                font: {
+                    size: 13,
+                    family: "Source Sans Pro",
+                    lineHeight: 1.2,
+                    style: "normal",
+                    weight: "700",
+
+                },
+                color:{},
+                usePointStyle: true,
+                padding: 20
+            }
+        },
+
+    }
+}
+
+
+export default options;
